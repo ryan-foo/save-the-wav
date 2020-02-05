0c9b4d67674607b8a00d20670d6df08261848a4c
Ryan <ryanicale@gmail.com>
2020-02-05 11:19:18 +0800

    Added time functions to measure how quickly the model runs.

--- QueueSaveTheWav.py
+++ QueueSaveTheWav.py
@@ -6,31 +6,25 @@ Ensure you have already trained the model, and its labels, and placed it in the 
 
 # Imports
 
-import tensorflow as tf
-import collections
+# import tensorflow as tf
+# import collections
 from queue import Queue
-from threading import Thread
+# from threading import Thread
 import label_wav
 import numpy as np
 import time
-import os
+# import os
 import wave
 
 import pyaudio
-from scipy.io import wavfile
-
-'''
-Load the trained model
-'''
+# from scipy.io import wavfile
 
 '''
 Audio Listener. Write to a pyaudio.Stream() <Open Microphone>
 '''
 
-# Change between Live and saving the .wav.
-SAVE_THE_WAV = False
 run = True
-verbose = False
+verbose = True
 
 threshold = 0.5 #threshold for detection
 chunk = 16000 # Chunks of 16000 samples # How muc audio sample per frame are we going to display?
@@ -77,6 +71,9 @@ def get_audio_input_stream(callback):
 # Data Buffer for input waveform
 data = np.zeros(feed_samples, dtype='int16')
 
+# Storing predictions to do post-processing upon
+predictions = []
+
 print('Get audio, begin callback.')
 
 stream = get_audio_input_stream(callback)
@@ -98,18 +95,28 @@ stream.start_stream()
 try:
     while run:
         # Dequeue data
+        start_time_main_loop = time.time()
         data = q.get()
 
+        print("Queue size:", q.qsize())
+
         # Convert np int16 to .wav format
-        
+        start_time_write_to_wav = time.time()
+
         wf = wave.open(filename, 'wb')
         wf.setnchannels(channels)
         wf.setsampwidth(p.get_sample_size(sample_format))
         wf.setframerate(fs)
         wf.writeframes(b''.join(data))
+
+        stop_time_write_to_wav = time.time()
+
+        print("Writing to a .wav took ", round(stop_time_write_to_wav - start_time_write_to_wav, 2), " seconds to run.")
         # wf.close()
 
         # Label.wav takes .wav, labels, graph, input_name, output_name and how many labels you want, running inference to print the predictions.
+
+        start_time_prediction = time.time()
 
         prediction = label_wav.label_wav(wav='output.wav', 
             labels='conv_labels.txt',
@@ -118,14 +125,29 @@ try:
             output_name='labels_softmax:0', 
             how_many_labels=1)
 
-        if (verbose == True):
-            print(prediction) # (word, score, encoding in a tuple)
+        stop_time_prediction = time.time()
 
-        if (q.full()):
-            print("We're all full up fam!")
+        print("Prediction took ", round(stop_time_prediction - start_time_prediction, 2), " seconds to run.")
 
+        # if (prediction[1] > threshold):
+        #     predictions.append(prediction[2]) # Append encoding, push old encoding out if its above 5!
+        #     predictions.push()
+        # else:
+        #     predictions.append(0)
+
+        # if (verbose == True):
+        #     print(prediction) # (predicted word, score <predicted probability>, encoding in a tuple)
+        #     print(predictions) # Current list of predictions thus far
+
+        # Detects if threshold is passed, and if the prediction is not 'silence' or 'unknown'
         if (prediction[1] > threshold and prediction[2] != 0):
-            print('Detected the word ' + str(prediction[0]) + ' with {} confidence.'.format(prediction[1]))
+
+            print('Detected the word ' + str(prediction[0]) + ' with {} confidence.'.format(prediction[1])
+                )
+
+        stop_time_main_loop = time.time()
+        print ("One main loop takes ", round(stop_time_main_loop - start_time_main_loop, 2), " seconds to run")
+
 
 except (KeyboardInterrupt, SystemExit):
     stream.stop_stream()
